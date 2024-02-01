098df5c6948531bd4ab0a7c8c9eed85a73bcab71
Bob McAleavey <bobmcaleavey@gmail.com>
2024-02-01 10:27:52 -0500

    feat: add Pwa components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,26 @@
 <script>
-	import { Title } from '$components';
+	import { Pwa, Title } from '$components';
 	import { theme } from '$stores';
 	import '../app.pcss';
 </script>
 
-<Title base="" />
+<svelte:head>
+	<meta name="description" content="Template for Sveltekit Projects" />
+</svelte:head>
+<Pwa>
+	<link rel="icon" type="image/svg+xml" href="/icons/icon.svg" />
+	<link rel="alternate icon" href="/icons/icon-16x16.png" />
+	<link rel="apple-touch-icon" href="/icons/icon-apple-touch.png" />
+	<link rel="manifest" href="/manifest.json" />
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover"
+	/>
+	<slot name="theme">
+		<meta name="theme-color" content="#09090C" />
+	</slot>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+</Pwa>
+<Title base="Template - Sveltekit" />
 <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,18 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}','./node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		plugin(({ addVariant }) => {
+			addVariant('pwa', '@media all and (display-mode: standalone)');
+		})
+	]
 };
 
 module.exports = config;
