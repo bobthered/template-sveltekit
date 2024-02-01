fdc54ee1b33eddb40cc1bc9993d5dbacf95a79f3
Bob McAleavey <bobmcaleavey@gmail.com>
2024-02-01 09:48:49 -0500

    feat: add sveltewind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}','./node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {}
